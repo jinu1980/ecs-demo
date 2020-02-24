f74ee91d3ca862f11ec40668a6186af8ce94138d
IBM-Cloud-DevOps <60835744+IBM-Cloud-DevOps@users.noreply.github.com>
2020-02-24 15:35:31 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>Blue Green Deployment v6<?php echo phpversion(); ?>.</p>
+                <p>Blue Green Deployment new<?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
