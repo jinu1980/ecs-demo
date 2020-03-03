8a9484f7a425a1a0bcb1e6bb6e6f100283fab8eb
pradeep-sony <60911097+pradeep-sony@users.noreply.github.com>
2020-03-03 09:53:00 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@
             <div class="hero-unit">
                 
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>Release 1.0</p>
+                <p>Release 2.0</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
